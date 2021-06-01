b2bea965ae94c280b570a7688491780106396b76
voiciphil <voiciphil@gmail.com>
2021-06-01 14:53:27 +0900

    Set 학습 테스트 요구사항 2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -22,5 +24,11 @@ public class SetTest {
     @Test
     void size() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains(Integer input) {
+        assertThat(numbers.contains(input)).isTrue();
     }
 }
