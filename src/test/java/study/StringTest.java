e715ccbd4aed7c66d5b34cbfaa00d0e3b7e5a1f9
voiciphil <voiciphil@gmail.com>
2021-06-01 14:27:48 +0900

    String 학습 테스트 요구사항 1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,12 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] actual1 = "1".split(",");
+        String[] actual2 = "1,2".split(",");
+        assertThat(actual1).contains("1");
+        assertThat(actual2).containsExactly("1", "2");
+    }
 }
