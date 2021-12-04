46d9ded22dced716f58423912a9934aaa2573f79
voiciphil <voiciphil@gmail.com>
2021-12-05 00:12:05 +0900

    feat: Main 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,7 +2,17 @@ package baseball;
 
 public class BaseballGame {
 
-    public static void main(String[] args) {
+    public static final NumberBoard numberBoard = new NumberBoard();
 
+    public static void main(String[] args) {
+        while (true) {
+            String input = InputView.getInput();
+            Result result = numberBoard.calculateResult(input);
+            ResultView.printResult(result);
+
+            if (result.isSuccess()) {
+                break;
+            }
+        }
     }
 }
--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -6,8 +6,8 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public String getInput() {
-        System.out.println("숫자를 입력해 주세요: ");
+    public static String getInput() {
+        System.out.print("숫자를 입력해 주세요: ");
         return scanner.nextLine();
     }
 }
