801e26e5cf6f92db2a7b1ee3a53a716ff13dd41b
voiciphil <voiciphil@gmail.com>
2021-12-04 23:55:08 +0900

    feat: calculateResult 함수 구현

--- src/main/java/baseball/NumberBoard.java
+++ src/main/java/baseball/NumberBoard.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class NumberBoard {
@@ -24,5 +26,54 @@ public class NumberBoard {
         if (previousSize < set.size()) {
             number += randomNumber;
         }
+    }
+
+    public Result calculateResult(String input) {
+        int strike = calculateStrike(input);
+        int ball = calculateBall(input);
+
+        return new Result(strike, ball);
+    }
+
+    private int calculateStrike(String input) {
+        int strike = 0;
+
+        for (int i = 0; i < DIGIT; i++) {
+            strike += isEqual(input.charAt(i), number.charAt(i));
+        }
+
+        return strike;
+    }
+
+    private int isEqual(char ch1, char ch2) {
+        if (ch1 == ch2) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    private int calculateBall(String input) {
+        int ball = 0;
+
+        for (int i = 0; i < DIGIT; i++) {
+            ball = isBall(input, i);
+        }
+
+        return ball - calculateStrike(input);
+    }
+
+    private int isBall(String input, int digit) {
+        int isBall = 0;
+
+        for (int i = 0; i < DIGIT; i++) {
+            isBall += isEqual(input.charAt(digit), number.charAt(i));
+        }
+
+        if (isBall > 0) {
+            return 1;
+        }
+
+        return 0;
     }
 }
